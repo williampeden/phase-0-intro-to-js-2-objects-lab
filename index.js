8e7e52dd6730690b2a4d8deeb088d3717c8615d7
William <wgepeden@gmail.com>
2021-11-07 18:27:33 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee = {
+    name: 'William Peden',
+    streetAddress: '117 Thomas St.',
+  };
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newemployee= { ...employee };
+  
+    newemployee[key] = value;
+  
+    return newemployee;
+  }
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+  
+    return employee;
+  } 
+  function deleteFromEmployeeByKey(employee, key, value) {
+      const newemployee= {...employee};
+      delete newemployee.name;
+      return newemployee;
+  }
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+      delete employee.name;
+      return employee;
+  }
